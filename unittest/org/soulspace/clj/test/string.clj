fb627e0962f315c92875d65c202e921a0d51f8a0
Ludger Solbach <soulman@soulspace.org>
2013-01-08 18:26:00 +0100

    enhanced unit test

--- unittest/org/soulspace/clj/test/string.clj
+++ unittest/org/soulspace/clj/test/string.clj
@@ -1,2 +1,36 @@
-(ns org.soulspace.clj.test.string)
+(ns org.soulspace.clj.test.string
+  (:use
+    [clojure.test]
+    [org.soulspace.clj.string]))
 
+(deftest gt-test
+  (is (true? (gt "b" "a")))
+  (is (true? (gt "ab" "aa")))
+  (is (false? (gt "a" "a")))
+  (is (false? (gt "aa" "aa")))
+  (is (false? (gt "a" "b")))
+  (is (false? (gt "aa" "ab"))))
+
+(deftest ge-test
+  (is (true? (ge "b" "a")))
+  (is (true? (ge "ab" "aa")))
+  (is (true? (ge "a" "a")))
+  (is (true? (ge "aa" "aa")))
+  (is (false? (ge "a" "b")))
+  (is (false? (ge "aa" "ab"))))
+
+(deftest lt-test
+  (is (false? (lt "b" "a")))
+  (is (false? (lt "ab" "aa")))
+  (is (false? (lt "a" "a")))
+  (is (false? (lt "aa" "aa")))
+  (is (true? (lt "a" "b")))
+  (is (true? (lt "aa" "ab"))))
+
+(deftest le-test
+  (is (false? (le "b" "a")))
+  (is (false? (le "ab" "aa")))
+  (is (true? (le "a" "a")))
+  (is (true? (le "aa" "aa")))
+  (is (true? (le "a" "b")))
+  (is (true? (le "aa" "ab"))))
