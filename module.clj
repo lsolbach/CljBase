d1ceacbc8a361b473ca42fe5e466908f3101fda5
Ludger Solbach <soulman@soulspace.org>
2013-07-07 12:32:43 +0200

    updated dependencies

--- module.clj
+++ module.clj
@@ -5,5 +5,5 @@
  :version "0.3.0"
  :description "CljLibrary is a library for clojure providing convenience functions with no other dependencies."
  :plugins ["global" "deps" "clojure" "package"]
- :dependencies [["org.clojure" "clojure" "1.3.0"]]
+ :dependencies [["org.clojure" "clojure" "1.5.1"]]
  ]
