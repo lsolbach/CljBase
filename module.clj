61c53e021caf00e2739e14c2e80dc5738a930bd0
Ludger Solbach <soulman@soulspace.org>
2013-07-26 03:41:31 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -1,12 +1,12 @@
 [
- :name "CljSwingLibrary"
+ :module "CljSwingLibrary"
  :project "org.soulspace.clj"
- :type "library"
+ :type :library
  :version "0.2.0"
  :description "A library for building Java Swing user interfaces in Clojure"
- :plugins ["global" "deps" "clojure" "package"]
- :dependencies [["org.clojure" "clojure" "1.3.0"]
-                ["org.soulspace.clj" "CljLibrary" "0.3.0"]
-                ["org.soulspace.clj" "CljJavaLibrary" "0.2.0"]
-                ["com.miglayout" "miglayout" "3.7.4"]]
+ :plugins ["global" "sdeps" "clojure" "package"]
+ :dependencies [[["org.clojure" "clojure" "1.5.1"]]
+                [["org.soulspace.clj" "CljLibrary" "0.3.0"]]
+                [["org.soulspace.clj" "CljJavaLibrary" "0.2.0"]]
+                [["com.miglayout" "miglayout" "3.7.4"]]]
  ]
