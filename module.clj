6dd00047df45b2a587b8c471b719bdf98a8ef29d
Ludger Solbach <soulman@soulspace.org>
2013-07-26 03:39:26 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -1,9 +1,9 @@
 [
- :name "CljLibrary"
+ :module "CljLibrary"
  :project "org.soulspace.clj"
- :type "library"
+ :type :library
  :version "0.3.0"
  :description "CljLibrary is a library for clojure providing convenience functions with no other dependencies."
- :plugins ["global" "deps" "clojure" "package"]
- :dependencies [["org.clojure" "clojure" "1.5.1"]]
+ :plugins ["global" "sdeps" "clojure" "package"]
+ :dependencies [[["org.clojure" "clojure" "1.5.1"]]]
  ]
