02cf6dacae1dfd1a5ad076a132b98dcd4db36458
Ludger Solbach <soulman@soulspace.org>
2013-08-03 06:57:01 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,7 +4,7 @@
  :type :library
  :version "0.3.0"
  :description "Clojure Java integration library"
- :plugins ["global" "dependencies" "clojure" "package"]
+ :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
                 [["org.soulspace.clj" "CljLibrary" "0.4.0"]]]
  ]
