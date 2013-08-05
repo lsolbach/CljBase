ba28b91a42d3ae14ebc58ca63a70562aea2b3259
Ludger Solbach <soulman@soulspace.org>
2013-08-05 02:49:27 +0200

    added copyright notice in source files

--- src/org/soulspace/clj/java/awt/geometry.clj
+++ src/org/soulspace/clj/java/awt/geometry.clj
@@ -1,3 +1,12 @@
+;
+;   Copyright (c) Ludger Solbach. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file license.txt at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+;
 (ns org.soulspace.clj.java.awt.geometry
   (:import [java.awt.geom Arc2D$Double CubicCurve2D$Double Dimension2D Ellipse2D$Double Line2D$Double Point2D$Double Rectangle2D$Double RoundRectangle2D$Double QuadCurve2D$Double]))
 
