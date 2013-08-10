7755c3e67e72e8753381dd88231108d9c1c108e6
Ludger Solbach <soulman@soulspace.org>
2013-08-10 05:45:46 +0200

    added dynamic classpath functionality, updated module files

--- module.clj
+++ module.clj
@@ -4,8 +4,8 @@
  :type :library
  :version "0.5.1"
  :description "The CljApplicationLibrary contains abstractions and functions for building applications in clojure."
- :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
+ :plugins ["global" "dependencies" "clojure" "package"] ;  "clojuretest"
  :dependencies [["org.clojure/clojure, 1.5.1"]
                 ["org.soulspace.clj/CljLibrary, 0.5.0"]
-                ]
+                ["org.tcrawley/dynapath, 0.2.3"]] ; dynamic classpath support
  ]
