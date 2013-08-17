1323418238ca8370f3b558d8556f6ea7f4c6ebb4
Ludger Solbach <soulman@soulspace.org>
2013-08-17 13:08:52 +0200

    cleanups

--- src/org/soulspace/clj/application/classpath.clj
+++ src/org/soulspace/clj/application/classpath.clj
@@ -78,4 +78,3 @@ if it is not dynamic already."
     (if (or (= (type cl) URLClassLoader) (= (type cl) DynamicClassLoader))
       (let [dyn-cl (create-dynamic-classloader cl)]
         (.setContextClassLoader (java.lang.Thread/currentThread) dyn-cl)))))
-
