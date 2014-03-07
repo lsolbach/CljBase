546bd210a4bcbd1ab70c24908f2a3a49b324f562
Ludger Solbach <soulman@soulspace.org>
2014-03-07 09:06:15 +0100

    updated module.clj

--- module.clj
+++ module.clj
@@ -5,9 +5,13 @@
  :version "0.2.0"
  :description "A library for building Java Swing user interfaces in Clojure"
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/ClojurePlugin"]
+           ["org.soulspace.baumeister/ClojureTestPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
  :dependencies [["org.clojure/clojure, 1.5.1"]
                 ["swing-utils/swing-utils, 0.2.0"]
-                ["org.soulspace.clj/CljJavaLibrary, 0.2.0"]
+                ["org.soulspace.clj/CljJavaLibrary, 0.5.0"]
                 ["com.miglayout/miglayout, 3.7.4"]]
  ]
