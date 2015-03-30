6a0e39bf7e974c0f0080c875c885629ec0d2fa2f
Ludger Solbach <soulman@soulspace.org>
2015-03-30 22:26:33 +0200

    added type hints, refactorings

--- CljSwingLibrary/src/org/soulspace/clj/java/print.clj
+++ CljSwingLibrary/src/org/soulspace/clj/java/print.clj
@@ -1,8 +1,10 @@
 (ns org.soulspace.clj.java.print
-  (:import [java.awt.print PrinterJob PageFormat Printable]
-           [javax.print DocFlavor DocFlavor$BYTE_ARRAY DocFlavor$CHAR_ARRAY DocFlavor$INPUT_STREAM
+  (:import [java.awt Graphics]
+           [java.awt.print PrinterJob PageFormat Printable]
+           [java.io OutputStream]
+           [javax.print Doc DocFlavor DocFlavor$BYTE_ARRAY DocFlavor$CHAR_ARRAY DocFlavor$INPUT_STREAM
                         DocFlavor$READER DocFlavor$SERVICE_FORMATTED DocFlavor$STRING DocFlavor$URL
-                        PrintServiceLookup ServiceUI ServiceUIFactory SimpleDoc
+                        DocPrintJob PrintService PrintServiceLookup ServiceUI ServiceUIFactory SimpleDoc
                         StreamPrintService StreamPrintServiceFactory]
            [javax.print.attribute Attribute AttributeSet HashPrintRequestAttributeSet]
            [javax.print.attribute.standard MediaSize MediaSizeName])
@@ -19,23 +21,30 @@
 
 (defn mime-type
   "Returns the mime type of the doc flavor."
-  [doc-flavor]
+  [^DocFlavor doc-flavor]
   (.getMimeType doc-flavor))
 
 (defn add-attribute
   "Adds the attribute to the attribute set."
-  [attr-set attr]
+  [^AttributeSet attr-set ^Attribute attr]
   (.add attr-set attr))
 
 (defn print-request-attribute-set
   "Creates a print request attribute set and adds the attributes if given."
-  ([]
+  (^AttributeSet []
     (HashPrintRequestAttributeSet.))
-  ([coll]
+  (^AttributeSet [coll]
     (let [attr-set (print-request-attribute-set)]
       (doseq [attr coll]
         (add-attribute attr-set attr))
       attr-set)))
+
+(defn printable
+  "Creates a Printable object."
+  (^Printable [f]
+    (proxy [Printable] []
+      (print [^Graphics gfx ^PageFormat page-format page-idx]
+        (f gfx page-format page-idx)))))
 
 (defn lookup-stream-print-service-factories
   "Returns the stream print service factories."
@@ -44,7 +53,7 @@
 
 (defn get-stream-print-service
   "Returns the stream print service from the stream print service factory for the output stream."
-  [factory out]
+  [^StreamPrintServiceFactory factory ^OutputStream out]
   (.getPrintService factory out))
 
 (defn lookup-default-print-service
@@ -54,40 +63,34 @@
 
 (defn lookup-print-services
   "Returns the print services compatible with the doc flavour and the attribute set."
-  ([^javax.print.DocFlavor doc-flavor ^javax.print.attribute.AttributeSet attr-set]
+  ([^DocFlavor doc-flavor ^AttributeSet attr-set]
     (PrintServiceLookup/lookupPrintServices doc-flavor attr-set)))
 
 (defn print-dialog
   "Shows the print dialog and returns the print service. If no print service is returned, the print must be cancelled."
-  ([x y print-services default-print-service attr-set]
+  ([x y #^PrintService print-services ^PrintService default-print-service ^AttributeSet attr-set]
     ; TODO lookup api signatures
     (ServiceUI/printDialog nil x y print-services default-print-service nil attr-set)))
 
 (defn create-print-job
   "Creates a print job on the print service."
-  [print-service]
+  ^DocPrintJob [^PrintService print-service]
   (.createPrintJob print-service))
 
 (defn simple-doc
   "Creates a simple doc for printing."
-  ([print-data ^javax.print.DocFlavor doc-flavor ^javax.print.attribute.AttributeSet attr-set]
-    (SimpleDoc. print-data doc-flavor attr-set)))
+  (^ SimpleDoc [^Object data ^DocFlavor doc-flavor ^AttributeSet attr-set]
+    (SimpleDoc. print doc-flavor attr-set)))
 
 (defn print-document
   "Print the document with the specified attribute set on the print job."
-  [print-job doc attr-set]
+  [^DocPrintJob print-job ^Doc doc ^AttributeSet attr-set]
   (.print print-job doc attr-set))
 
 (defn print-data
   "Print the data with the specified attribute set on the print service."
-  [print-service data doc-flavor attr-set]
+  [^PrintService print-service ^Object data ^DocFlavor doc-flavor ^AttributeSet attr-set]
   (let [print-job (create-print-job print-service)
         doc (simple-doc data doc-flavor attr-set)]
     (print-document print-job doc attr-set)))
 
-(defn create-printable
-  "Creates a Printable object."
-  [f]
-  (proxy [Printable] []
-    (print [gfx page-format page-idx]
-      (f gfx page-format page-idx))))
