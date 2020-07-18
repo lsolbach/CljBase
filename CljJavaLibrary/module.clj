6ae97316b3fdbead35de295bd500b84de0450558
Ludger Solbach <soulman@soulspace.org>
2020-07-18 10:13:06 +0200

    formatting

--- CljJavaLibrary/module.clj
+++ CljJavaLibrary/module.clj
@@ -10,8 +10,7 @@
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :plugins [["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
-           ["org.soulspace.baumeister/PackagePlugin"]
-           ]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+
  :dependencies [["org.clojure/clojure, 1.8.0"]
-                ["org.soulspace.clj/CljLibrary, 0.7.0"]]
- ]
+                ["org.soulspace.clj/CljLibrary, 0.7.0"]]]
