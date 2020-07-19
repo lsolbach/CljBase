2413feff10978de68b5f9fc7f0bcd3b306c4a7f8
Ludger Solbach <soulman@soulspace.org>
2020-07-20 01:43:51 +0200

    cleanups and comment updates, require namespaces instead of use

--- CljMailLibrary/src/org/soulspace/clj/mail/mail.clj
+++ CljMailLibrary/src/org/soulspace/clj/mail/mail.clj
@@ -1,16 +1,20 @@
-;
-;   Copyright (c) Ludger Solbach. All rights reserved.
-;   The use and distribution terms for this software are covered by the
-;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;   which can be found in the file license.txt at the root of this distribution.
-;   By using this software in any fashion, you are agreeing to be bound by
-;   the terms of this license.
-;   You must not remove this notice, or any other, from this software.
-;
+;;
+;;   Copyright (c) Ludger Solbach. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;   which can be found in the file license.txt at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+;;
 (ns org.soulspace.clj.mail.mail
+  (:require [org.soulspace.clj.java.beans :as b])
   (:import [javax.mail MessageRemovedException Session Transport]
-           [javax.mail.internet MimeMessage MimeUtility])
-  (:use [org.soulspace.clj.java beans properties]))
+           [javax.mail.internet MimeMessage MimeUtility]))
+
+;;
+;; Functions to send mails with the JavaX Mail API
+;;
 
 (def ^:dynamic *mail-session*)
 
@@ -25,7 +29,7 @@
    (mime-message *mail-session* props))
   ([session props]
    (let [msg (MimeMessage. session)]
-     (set-properties! msg props)
+     (b/set-properties! msg props)
      msg)))
 
 (defn send-mail
